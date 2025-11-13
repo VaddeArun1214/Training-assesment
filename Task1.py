cd47cb9f34b5469331c930064ce2bf3673bf2425
VaddeArun1214 <vaddearun479@gmail.com>
2025-11-13 14:41:13 +0530

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,5 +1,13 @@
-a = int(input())
-if (a % 2 == 0):
-    print("True")
-else:
-    print("False")
+def prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+print(prime(4)) # False
+print(prime(2)) # True
+
